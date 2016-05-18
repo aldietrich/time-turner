65dbf4a53445eee914339399716a6895769605fc
Garrick Rohm <garrick.rohm@gmail.com>
2016-05-18 08:57:48 -0500

    Update app.js
    Adding Epoch converter

--- app.js
+++ app.js
@@ -7,6 +7,15 @@
 
     doSomething: function() {
     }
+    //Garrick
+    //This function will take a Date in any of the formats I posted in Skype and return an Epoch Timestamp
+    function getEpochTime(Input) {
+      var inputDate = new Date(Input)
+      var myEpoch = inputDate.getTime()/1000.0;
+      return myEpoch;
+      //Uncomment this for console debugging
+      //console.log(myEpoch)
+      }
   };
 
 }());
